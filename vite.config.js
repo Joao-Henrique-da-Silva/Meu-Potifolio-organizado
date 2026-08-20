0629a656eff9153f77c18da296f65f5622ae27e0
Joao Henrique da Silva <joaohs22@gmail.com>
2026-08-20 14:52:58 -0300

    Corrigindo base do Vite para o nome do repositório

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Meu-Potifolio-organizado/', // ← APENAS o nome do repositório
 })
